003135ad64fb0672c31541b1d6577c5d29e2c5e6
Akbarshox <akbarshax@bk.ru>
2019-12-06 18:41:28 +0500

    card

--- xona.js
+++ xona.js
@@ -4,6 +4,10 @@ const axios = require('axios');
 var LocalStorage = require('node-localstorage').LocalStorage;
 localStorage = new LocalStorage('./scratch');
 
+
+var Data = require('./kartalar.json');
+
+
 // replace the value below with the Telegram token you receive from @BotFather
 const token = '1040511526:AAGwUViQakRsGqQOYrd4R-mfMj1wEy0nE78';
  
@@ -15,7 +19,6 @@ bot.onText(/\/start/, function (msg, match) {
     const chatId = msg.chat.id;
 });
         
-
 
 bot.onText(/\/musor/, (msg, match) => {
 
@@ -43,7 +46,85 @@ bot.onText(/\/ovqat/, (msg, match) => {
     caption: "ovqat"
   });
 
-})  
+}) 
+
+bot.onText(/\/ovqat/, (msg, match) => {
+
+  const chatId = msg.chat.id;
+
+  const chatID = msg.chat.id;
+  const photo = `4.jpg`;
+  bot.sendPhoto(chatID, photo, {
+    caption: "non"
+  });
+
+})
+
+
+bot.onText(/\/kartalar/, (msg, match) => {
+ 
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, 'karta raqamalri', {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: 'Azizbek',
+            callback_data: 'Azizbek'  
+          },
+          {
+            text: 'Makhmudjon',
+            callback_data: 'Makhmudjon'  
+          },
+          {
+            text: 'Akbarshox',
+            callback_data: 'Akbarshox'  
+          },
+          {
+            text: 'Khumoyun',
+            callback_data: 'Khumoyun'  
+          }
+        ]
+      ]
+    }
+
+
+  });
+
+  bot.on('callback_query', query => {
+    const id = query.message.chat.id
+    
+    // request('Data', function (error, response, body) {
+    //   console.log('error:', error); // Print the error if one occurred
+    //   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    //   console.log('body:', body); // Print the HTML for the Google homepage.
+    // });
+
+        // request('http://cbu.uz/ru/arkhiv-kursov-valyut/json/', function(err, response, body) {
+          const data = Data;
+          const result = data.filter(item => item.name == query.data)[0];
+          console.log(result)
+          let md = `
+              Date: _${result.name}_
+              Pokupka: _${result.number}_
+          `;
+          
+          // function copyToClipboard(element) {
+          //   var $temp = $("<input>");
+          //   $("body").append($temp);
+          //   $temp.val($(element).text()).select();
+          //   document.execCommand("copy");
+          //   $temp.remove();
+          // }
+          // copyToClipboard(result.number)
+
+          bot.sendMessage(id, md, {parse_mode: 'Markdown'});
+        })
+      })
+    // })
+  // })
+
 
 
 
